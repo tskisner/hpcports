96b80a12239f57be7af7b83ec4b415bb4f676e6f
Theodore Kisner <tskisner.public@gmail.com>
2013-07-23 13:26:47 -0700

    fix carver initialization

--- system/carver-gnu-dbg.sh
+++ system/carver-gnu-dbg.sh
@@ -1,8 +1,9 @@
-loaded=`${MODULESHOME}/bin/modulecmd sh -t list 2>&1 | grep openmpi-gnu`
-if [ "x${loaded}" = x ]; then
+ver=`gcc --version | head -n 1 | awk '{ print $3 }'`
+if [ "x${ver}" != "x4.7.0" ]; then
     module unload pgi
     module load gcc/4.7.0
     module unload openmpi
     module load openmpi-gnu/1.4.5
     module load mkl/10.2
+    module load git/1.7.8.3
 fi
