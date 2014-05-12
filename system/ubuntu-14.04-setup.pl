586fa8778fca2ed1de2bf734c17b0498b23a6d65
Theodore Kisner <tskisner.public@gmail.com>
2014-05-11 23:21:59 -0700

    add setup files for ubuntu 14.04 with maximal overrides.

---
+++ system/ubuntu-14.04-setup.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/env perl
+
+use strict;
+
+# List of dependencies
+
+my $DEPS = {
+"environment-modules" => "",
+"python-astropy" => "astropy",
+"autoconf" => "autoconf",
+"automake" => "automake",
+"libopenmpi-dev" => "openmpi",
+"libopenblas-dev" => "blas",
+"libboost1.55-all-dev" => "boost",
+"bzip2" => "bzip2",
+"libbz2-dev" => "",
+"libcfitsio3-dev" => "cfitsio",
+"cmake" => "cmake",
+"libcurl4-openssl-dev" => "curl",
+"cython" => "cython",
+"libeigen3-dev" => "eigen",
+"libexpat1-dev" => "expat",
+"libfftw3-dev" => "fftw",
+"gdb" => "gdb",
+"git" => "git",
+"libgsl0-dev" => "gsl",
+"gzip" => "gzip",
+"python-h5py" => "h5py",
+"libhdf5-dev" => "hdf5",
+"ipython" => "ipython",
+"liblapack-dev" => "lapack",
+"libarchive-dev" => "libarchive",
+"libjpeg-dev" => "libjpeg",
+"libtiff5-dev" => "libtiff",
+"libtool" => "libtool",
+"m4" => "m4",
+"python-matplotlib" => "matplotlib",
+"python-mpi4py" => "mpi4py",
+"mysql-client" => "mysql_client",
+"python-mysqldb" => "mysql_python",
+"libnetcdf-dev" => "netcdf",
+"python-numpy" => "numpy",
+"python-numexpr" => "numexpr",
+"libpcre3-dev" => "pcre",
+"libplplot-dev" => "plplot",
+"postgresql-client" => "postgresql",
+"python-psycopg2" => "psycopg2",
+"python-pyfits" => "pyfits",
+"python-scientific" => "scientific",
+"python-scipy" => "scipy",
+"scons" => "scons",
+"screen" => "screen",
+"python-setuptools" => "setuptools",
+"sfftw-dev" => "sfftw",
+"sqlite" => "",
+"libsqlite3-dev" => "sqlite",
+"swig" => "swig",
+"tcsh" => "tcsh",
+"tcl8.6" => "tcl",
+"tk8.6" => "tk",
+"python-urwid" => "urwid",
+"valgrind" => "valgrind",
+"wcslib-dev" => "wcslib",
+"libwcstools-dev" => "wcstools",
+"xpa-tools" => "xpa",
+"libxpa1" => "",
+"zlib1g-dev" => "zlib" };
+
+use Data::Dumper;
+
+#print Dumper ( $DEPS );
+
+# install
+
+my $install = join ( " ", keys( %{$DEPS} ) );
+system( "sudo apt-get install ${install}" );
+
+# generate override info
+
+system( "cp ubuntu-14.04.make.in ubuntu-14.04.make" );
+
+open(OUT, ">>ubuntu-14.04.make");
+
+foreach my $linux ( keys %{$DEPS} ) {
+	my $hpcp = $DEPS->{$linux};
+	if ( $hpcp ne "" ) {
+		my $ver = `apt-cache show ${linux} | grep -e "^Version*"`;
+		$ver =~ s/(.*)\n.*/$1/;
+		chomp($ver);
+		$ver =~ s/.*:\s*([^+^-]*).*/$1/;
+		print OUT "${hpcp}_OVERRIDE = TRUE\n";
+		print OUT "${hpcp}_PREFIX = \$(root)\n";
+		print OUT "${hpcp}_VERSION = ${ver}\n";
+		print OUT "\n";
+	}
+}
+
+close (OUT);
