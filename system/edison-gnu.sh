07e1301149fda39edba40537f0183c54f162e265
Theodore Kisner <tskisner.public@gmail.com>
2016-08-26 15:51:40 -0700

    Bump edison to use gcc-6.1.0

--- system/edison-gnu.sh
+++ system/edison-gnu.sh
@@ -9,9 +9,9 @@ if [ "x${loadedgnu}" = x ]; then
     if [ "x${loadedintel}" != x ]; then
       module swap PrgEnv-intel PrgEnv-gnu
     fi
-    module swap gcc gcc/5.3.0
+    module swap gcc gcc/6.1.0
     module load intel
-    module load git/2.4.6
+    module load git
     module load zlib/1.2.7
     module load curl/7.28.1
     module load bzip2/1.0.6
